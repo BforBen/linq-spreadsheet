500d94b6a1bde7adf9508b7245683914c9380648
Ben <BforBen@users.noreply.github.com>
2016-07-08 14:30:20 +0100

    Add support for IDictionary

--- GuildfordBoroughCouncil.Linq.Spreadsheet/Spreadsheet.cs
+++ GuildfordBoroughCouncil.Linq.Spreadsheet/Spreadsheet.cs
@@ -12,14 +12,14 @@ namespace GuildfordBoroughCouncil.Linq
 {
     public static class Spreadsheet
     {
-        private static ExcelFile GenerateSpreadsheet<T>(IEnumerable<T> list = null, InformationProtectiveMarking.Distribution Distribution = InformationProtectiveMarking.Distribution.Internal, string HeaderText = "")
+        private static ExcelFile GenerateSpreadsheet<T>(IEnumerable<T> list = null, InformationProtectiveMarking.Distribution Distribution = InformationProtectiveMarking.Distribution.Internal, string Title = "")
         {
             SpreadsheetInfo.SetLicense(Properties.Settings.Default.GemBoxSpreadsheetLicenseKey);
             SpreadsheetInfo.FreeLimitReached += (object sender, FreeLimitEventArgs e) => { e.FreeLimitReachedAction = FreeLimitReachedAction.ContinueAsTrial; };
 
             var ef = new ExcelFile();
 
-            ef.DocumentProperties.BuiltIn[BuiltInDocumentProperties.Title] = "Asset list as at " + DateTime.Now.ToLongDateString();
+            ef.DocumentProperties.BuiltIn[BuiltInDocumentProperties.Title] = Title;
 
             ef.DocumentProperties.Custom.Add("bjDocumentLabelXML", @"<?xml version=""1.0"" encoding=""us-ascii""?><sisl xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" sislVersion=""0"" policy=""b62b33be-a5f6-4f9d-86d0-ef32eac2404f"" xmlns=""http://www.boldonjames.com/2008/01/sie/i");
             ef.DocumentProperties.Custom.Add("bjDocumentLabelXML-0", @"nternal/label""><element uid=""id_protective_marking_new_item_1"" value="""" /><element uid=""id_distribution_newvalue1"" value="""" /></sisl>");
@@ -32,7 +32,7 @@ namespace GuildfordBoroughCouncil.Linq
 
             var hf = ws.HeadersFooters;
 
-            hf.FirstPage.Header.CenterSection.Content = HeaderText;
+            hf.FirstPage.Header.CenterSection.Content = Title;
             hf.DefaultPage.Header = hf.FirstPage.Header;
 
             hf.FirstPage.Footer.CenterSection.Append("Page ").Append(HeaderFooterFieldType.PageNumber).Append(" of ").Append(HeaderFooterFieldType.NumberOfPages);
@@ -162,23 +162,153 @@ namespace GuildfordBoroughCouncil.Linq
             return ef;
         }
 
-        public static ExcelFile GetSpreadsheet<T>(IEnumerable<T> list = null, string fileName = "report.ods", string HeaderText = "")
+        public static ExcelFile GetSpreadsheet<T>(IEnumerable<T> list = null, string fileName = "report.ods", string Title = "")
         {
-            return GenerateSpreadsheet(list, HeaderText: HeaderText);
+            return GenerateSpreadsheet(list, Title: Title);
         }
 
-        public static void AsSpreadsheet<T>(this HttpResponseBase httpResponse, IEnumerable<T> list = null, string fileName = "report.ods", string HeaderText = "")
+        public static void AsSpreadsheet<T>(this HttpResponseBase httpResponse, IEnumerable<T> list = null, string fileName = "report.ods", string Title = "")
         {
-            var ef = GenerateSpreadsheet(list, HeaderText: HeaderText);
+            var ef = GenerateSpreadsheet(list, Title: Title);
 
             ef.Save(httpResponse, fileName);
         }
 
-        public static void ToSpreadsheet<T>(this IEnumerable<T> list, string fileName = "report.ods", string HeaderText = "")
+        public static void ToSpreadsheet<T>(this IEnumerable<T> list, string fileName = "report.ods", string Title = "")
         {
-            var ef = GenerateSpreadsheet(list, HeaderText: HeaderText);
+            var ef = GenerateSpreadsheet(list, Title: Title);
             
             ef.Save(fileName);
         }
+
+        #region IDictionary
+
+        private static ExcelFile GenerateSpreadsheet(IEnumerable<IDictionary<string, object>> list = null, InformationProtectiveMarking.Distribution Distribution = InformationProtectiveMarking.Distribution.Internal, string Title = "")
+        {
+            if (list == null || list.Count() == 0)
+            {
+                return null;
+            }
+
+            SpreadsheetInfo.SetLicense(Properties.Settings.Default.GemBoxSpreadsheetLicenseKey);
+            SpreadsheetInfo.FreeLimitReached += (object sender, FreeLimitEventArgs e) => { e.FreeLimitReachedAction = FreeLimitReachedAction.ContinueAsTrial; };
+
+            var ef = new ExcelFile();
+
+            ef.DocumentProperties.BuiltIn[BuiltInDocumentProperties.Title] = Title;
+
+            ef.DocumentProperties.Custom.Add("bjDocumentLabelXML", @"<?xml version=""1.0"" encoding=""us-ascii""?><sisl xmlns:xsd=""http://www.w3.org/2001/XMLSchema"" xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" sislVersion=""0"" policy=""b62b33be-a5f6-4f9d-86d0-ef32eac2404f"" xmlns=""http://www.boldonjames.com/2008/01/sie/i");
+            ef.DocumentProperties.Custom.Add("bjDocumentLabelXML-0", @"nternal/label""><element uid=""id_protective_marking_new_item_1"" value="""" /><element uid=""id_distribution_newvalue1"" value="""" /></sisl>");
+
+            ef.DocumentProperties.Custom.Add("bjDocumentSecurityLabel", "Guildford Borough Council UNCLASSIFIED INTERNAL");
+
+            var ws = ef.Worksheets.Add("Sheet1");
+
+            #region Header and footer
+
+            var hf = ws.HeadersFooters;
+
+            hf.FirstPage.Header.CenterSection.Content = Title;
+            hf.DefaultPage.Header = hf.FirstPage.Header;
+
+            hf.FirstPage.Footer.CenterSection.Append("Page ").Append(HeaderFooterFieldType.PageNumber).Append(" of ").Append(HeaderFooterFieldType.NumberOfPages);
+            hf.DefaultPage.Footer = hf.FirstPage.Footer;
+
+            #endregion
+
+            #region Print options
+
+            var printOptions = ws.PrintOptions;
+            printOptions.Portrait = false;
+
+            #endregion
+
+            #region Header row
+
+            var i = 0;
+
+            var DataFormat = new Dictionary<string, SpreadsheetDisplayFormatAttribute>();
+
+            foreach (var d in list.First())
+            {
+                string displayName = d.Key;
+                
+                ws.Cells[0, i].Value = displayName;
+
+                i++;
+            }
+
+            var HeaderStyle = new CellStyle();
+            HeaderStyle.HorizontalAlignment = HorizontalAlignmentStyle.Left;
+            HeaderStyle.FillPattern.SetSolid(ColorTranslator.FromHtml("#538DD5"));
+            HeaderStyle.Font.Weight = ExcelFont.BoldWeight;
+            HeaderStyle.Font.Color = Color.White;
+
+            ws.Cells.GetSubrangeAbsolute(0, 0, 0, Math.Max(i - 1, 0)).Style = HeaderStyle;
+            // Freeze top row
+            ws.Panes = new WorksheetPanes(PanesState.Frozen, 0, 1, "A2", PanePosition.BottomLeft);
+
+            #endregion
+
+            if (list != null)
+            {
+                var rowId = 1;
+
+                foreach (var row in list)
+                {
+                    var cellId = 0;
+
+                    foreach (var k in row.Keys)
+                    {
+                        var value = row[k];
+
+                        var cell = ws.Cells[rowId, cellId];
+
+                        if (row[k].GetType().ToString().Contains("System.Int64"))
+                        {
+                            cell.Style = new CellStyle { NumberFormat = "0" };
+                        }
+
+                        cell.Value = value;
+
+                        cellId++;
+                    }
+
+                    rowId++;
+                }
+            }
+
+            if (ws.Rows.Count > 0)
+            {
+                // Autofit columns
+                for (i = 0; i < ws.CalculateMaxUsedColumns(); i++)
+                {
+                    ws.Columns[i].AutoFit(1.5, ws.Rows[0], ws.Rows[ws.Rows.Count - 1]);
+                }
+            }
+
+            return ef;
+        }
+
+        public static ExcelFile GetSpreadsheet(IEnumerable<IDictionary<string, object>> list = null, string fileName = "report.ods", string Title = "")
+        {
+            return GenerateSpreadsheet(list, Title: Title);
+        }
+
+        public static void AsSpreadsheet(this HttpResponseBase httpResponse, IEnumerable<IDictionary<string, object>> list = null, string fileName = "report.ods", string Title = "")
+        {
+            var ef = GenerateSpreadsheet(list, Title: Title);
+
+            ef.Save(httpResponse, fileName);
+        }
+
+        public static void ToSpreadsheet(this IEnumerable<IDictionary<string, object>> list, string fileName = "report.ods", string Title = "")
+        {
+            var ef = GenerateSpreadsheet(list, Title: Title);
+
+            ef.Save(fileName);
+        }
+
+        #endregion
     }
 }
